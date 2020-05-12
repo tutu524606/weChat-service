d72c399a0ec1c7e4b676922c7bea7d75ecca2cf6
tutu524606 <524682015@qq.com>
2020-05-12 11:26:30 +0800

    提交

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+###
+ # @Author: your name
+ # @Date: 2020-05-12 10:56:56
+ # @LastEditTime: 2020-05-12 11:26:03
+ # @LastEditors: Please set LastEditors
+ # @Description: In User Settings Edit
+ # @FilePath: /weChat-service/deploy.sh
+ ###
+git add .
+git commit -m '提交'
+git pull
+git push
